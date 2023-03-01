e5fa47162317450bd7d6e6cf6438370f77f1557a
AguiarIsaac <isaacaguiar_@hotmail.com>
2023-03-01 16:32:45 -0300

    validações page recover

--- src/routes/RecoverAccess/index.tsx
+++ src/routes/RecoverAccess/index.tsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
-import { SectionForm } from "./styles";
+import { FormError, SectionForm } from "./styles";
 import { z } from 'zod';
 import { useForm } from "react-hook-form";
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext, useState } from "react";
+import { AccessContext } from "../../contexts/AccessContext";
 
 const recoverFormSchema = z.object({
   email: z
@@ -15,10 +17,27 @@ type recoverFormData = z.infer<typeof recoverFormSchema>
 
 export function RecoverAccess() {
 
-  const {register, handleSubmit} = useForm<recoverFormData>({resolver: zodResolver(recoverFormSchema)})
+  const {
+    register, 
+    handleSubmit, 
+    reset,
+    formState: {isSubmitting}
+  } = useForm<recoverFormData>({resolver: zodResolver(recoverFormSchema)})
+
+  const context = useContext(AccessContext)
+  const [emailError, setEmailError] = useState('')
+  const [success, setSuccess] = useState('')
 
   function handleRecover(data: recoverFormData) {
-    console.log(data)
+    const emailValidate = context.user.email
+    if(data.email !== emailValidate) {
+      setEmailError('Email não cadastrado ou inválido, tente novamente ou vá para tela de cadastro.')
+      reset()
+    } else {
+      setSuccess('email enviado! verifique sua caixa de email e siga as instruções')
+      setEmailError('')
+      reset()
+    }
   }
 
 
@@ -26,13 +45,17 @@ export function RecoverAccess() {
     <SectionForm>
       <div className="box">
         <h2>Resetar Sua senha</h2>
-        <p><Link to='/'>Voltar para tela de login.</Link></p> 
+        <p><Link to='/'>Voltar para tela de login.</Link></p>
+
+        {emailError && (
+          <FormError>{emailError}</FormError>
+        )} 
         <form onSubmit={handleSubmit(handleRecover)}>
           <label>
             Email
             <input {...register('email')} type="email" name="email" id="email" />
           </label>
-          <button type="submit">Resetar Senha</button>
+          <button type="submit" disabled={isSubmitting}>Resetar Senha</button>
         </form>
       </div>
     </SectionForm>
--- src/routes/RecoverAccess/styles.ts
+++ src/routes/RecoverAccess/styles.ts
@@ -65,3 +65,12 @@ export const SectionForm = styled.section`
     }
   }
 `
+
+export const FormError = styled.span`
+
+  display: block;
+  margin-top:2rem;
+  margin-bottom: -3rem;
+  border-left: 2px solid #FF455C;
+  padding-left: 0.25rem;
+`
